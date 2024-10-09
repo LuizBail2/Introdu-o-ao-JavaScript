fd5c883937e39fbd7047671f8421ea8459d105f3
Luiz Bail <luizgustavobail72@gmail.com>
2024-10-08 22:53:40 -0300

    Calculadora com JavaScript

--- script.js
+++ script.js
@@ -1,94 +1,17 @@
-console.log("Aula 03 JavaScript");
-
-// Operadores artimeticos
-let x = 10;
-let y =5;
-
-let soma = x + y; 
-let subtracao = x- y;
-let multiplicacao = x * y; 
-let divisao = x / y;
-let resto = x % y;
-
-// Operadores de  atribuiçao
-let a = 10;
-a + 5;
-
-// Operadores de Comparação
-let = time = "Flamengo";
-let maiorTime = time == "Flamengo";
-console.log(maiorTime);
-
-// Estruturas Condicionais
-const idade =18;
-
-if (idade >=18) {
-    console.log("Você é maior de idade");
+function calculadora(a, b, operacao) {
+    if (operacao == 'soma') {
+        return a + b;
+    } else if (operacao == 'subtracao') {
+        return a - b;
+    } else if (operacao == 'multiplicacao') {
+        return a * b;
+    } else if (operacao == 'divisao') {
+        return a / b;
     } else {
-    console.log("Voce é menor de idade");
-}
-
-// Estrutura de repetição
-
-// Exercicio 1
-function VerificarIdade() {
-    const idade = document.getElementById("idade").value;
-    const resultado = document.getElementById("resultado");
-
-    if (idade >= 18) {
-        resultado.textContent = "Você é maior de idade.";
-        resultado.style.color = "green";
-        console.log("Você é maior de idade");
-    } else {
-        resultado.textContent = "Você é menor de idade.";
-        resultado.style.color = "red";
-        console.log("Você é menor de idade");
-        
+        return 'Operação inválida';
     }
 }
-
-
-// Exercicio 2
-function VerificarNumero() {
-    const numero = document.getElementById("numeros").value;
-
-    if (numero%2==0) {
-        console.log("Par");
-       
-    } else {
-        console.log("Impar");
-       
-    }
-}
-
-// Exercicio 3
-function VerificarPositivoNegativo() {
-    const Numero = document.getElementById("Numero").value;
-    const Verificação = document.getElementById("Verificação");
-
-    if (Numero==0) {
-        Verificação.textContent = "Seu Numero é Zero";
-        Verificação.style.color = "black";
-        console.log("Zero");
-    } else if (Numero >=1) {
-        Verificação.textContent = "Seu Numero é Positivo";
-        Verificação.style.color = "darkblue";
-        console.log("Positivo");
-}
-    else {
-        Verificação.textContent = "Seu Numero é Negativo";
-        Verificação.style.color = "red";
-        console.log("Negativo");
-        
-    }
-}
-
-// Exercicio 4
-function Tabuada(x) {
-    
-    for (let i = 1; i <11; i++) {
-        let total = x*i;
-        console.log(x + " x " + i + " = " + total);
-}
-}
-Tabuada(7)
+console.log(calculadora(5, 3, 'soma'))
+console.log(calculadora(63, 34, 'subtracao'))
+console.log(calculadora(23, 7, 'multiplicacao'))
+console.log(calculadora(81, 9, 'divisao'))
